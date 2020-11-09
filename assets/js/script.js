e48b76cf4de07ba7ae6080e8062ea7d50e64049a
Delph <delph_teb@hotmail.com>
2020-11-09 00:31:54 -0500

    add copy button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
+//TO DO
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Copy password into clipboard //TO FIX
+function myFunction() {
+  var copyText = document.querySelector("#password");
+  copyText.select();
+  document.execCommand("copy");
+}
+
+// Add event listener to copy button
+document.querySelector("#toCopy").addEventListener("click", myFunction);
