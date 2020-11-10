6ef3c954eb4096afde0cb23d274fa50b7b76bde4
Delph <delph_teb@hotmail.com>
2020-11-10 18:35:49 -0500

    add password lenght query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt for a number from 8 to 128
+function readNumber() {
+  let num;
+  do {
+    num = prompt("Enter the characters length of your password (8 to 128): ", 0);
+  } while (!isFinite(num) || !(num >= 8 && num <= 128));
+  return num;  
+}
 
 
-//TO DO
+// Prompt for character types
+function userChoice(val) {
+  let boolVal = confirm("Do you want " + val +"?"); 
+  return boolVal;
+}
 
+/******** Function to generate a password of x characters ********/
+
+
+
+
+
+
+
+
+
+
+/*********** End a password generator function ***********/
 
 // Write password to the #password input
 function writePassword() {
